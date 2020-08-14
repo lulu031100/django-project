d343beda70ca3b1a972ddbb41540cc6cde173ac1
lulu031100 <lulu031100@gmail>
2020-08-14 20:57:57 +0900

    0814

--- techpitgram/settings.py
+++ techpitgram/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'techpitgram',
-        'USER': 'root',
-        'PASSWORD': 'cvu255Mysql',
+        'USER': 'lucyuser',
+        'PASSWORD': 'cvu255+Mysql',
         'HOST': 'localhost',
         'PORT': '3306',
     }
